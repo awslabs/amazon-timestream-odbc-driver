ea1bfbe43da58827b96a700fc7338f19c64c181f
Trevor Bonas <Trevor.Bonas@improving.com>
2024-07-02 23:11:17 +0000

    Fix macOS and Linux failures

--- src/odbc/src/app/application_data_buffer.cpp
+++ src/odbc/src/app/application_data_buffer.cpp
@@ -22,6 +22,7 @@
 
 #include <algorithm>
 #include <codecvt>
+#include <locale>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -259,7 +260,7 @@ ConversionResult::Type ApplicationDataBuffer::PutStrToStrBuffer(
   if (ANSI_STRING_ONLY) {
     bytesRequired = value.length() * outCharSize;
   } else {
-    thread_local std::wstring_convert< std::codecvt_utf8< wchar_t >, wchar_t >
+    thread_local std::wstring_convert<std::codecvt_utf8<wchar_t >, wchar_t>
       converter;
     std::wstring inString = converter.from_bytes(value.c_str());
     bytesRequired = inString.length() * outCharSize;

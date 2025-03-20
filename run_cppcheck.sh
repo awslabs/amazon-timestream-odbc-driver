6d7a762993831ce236159492fea4d64f44ce5bd1
forestmvey <forestv@bitquilltech.com>
2025-03-20 15:13:10 -0700

    Updates for cppcheck.
    Signed-off-by: forestmvey <forestv@bitquilltech.com>

--- run_cppcheck.sh
+++ run_cppcheck.sh
@@ -17,11 +17,11 @@ RESULTS_FILE=cppcheck-results.log
 
 # --force: force checks all define combinations (default max is 12)
 # -iaws-sdk-cpp: avoid checking AWS C++ SDK source files in our repo
-cppcheck --check-level=exhaustive --force --library=boost --library=timestream -UWIN32 ./src/ 2>  ${RESULTS_FILE}
+cppcheck --check-level=exhaustive --force --library=boost --library=timestream -UWIN32 -isrc/vcpkg_installed ./src/ 2>${RESULTS_FILE}
 
 if [ -s ${RESULTS_FILE} ]; then
-    echo "!! Cppcheck errors found! Check ${RESULTS_FILE} for details."
-    exit 1
+  echo "!! Cppcheck errors found! Check ${RESULTS_FILE} for details."
+  exit 1
 else
-    echo "No Cppcheck errors found."
+  echo "No Cppcheck errors found."
 fi

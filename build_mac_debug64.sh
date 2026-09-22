2ddb319d47bb3198c63f95a173e415ba392ae288
Forest Vey <veforest@amazon.com>
2026-09-22 15:28:47 -0700

    Initial commit for fixing GH CI.
    Signed-off-by: Forest Vey <veforest@amazon.com>

--- build_mac_debug64.sh
+++ build_mac_debug64.sh
@@ -11,36 +11,31 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-#build AWS SDK static library
 cd src
-git clone --recurse-submodules -b "1.11.80" "https://github.com/aws/aws-sdk-cpp.git"
-cd aws-sdk-cpp
-mkdir install
-mkdir build
-cd build
-cmake ../ -DCMAKE_INSTALL_PREFIX="../install" -DTARGET_ARCH="APPLE" -DCMAKE_BUILD_TYPE="Debug" -DBUILD_ONLY="core;sts;timestream-query;timestream-write" -DCUSTOM_MEMORY_MANAGEMENT="OFF" -DENABLE_TESTING="OFF" -DBUILD_SHARED_LIBS="OFF"
-make -j 4
-make install
-cd ../../../
+vcpkg install
+cd ..
+
+VCPKG_INSTALLED_DIR='src/vcpkg_installed/x64-osx'
+if [[ $(uname -m) =~ ^arm.* || $(uname -m) == 'aarch64' ]]; then
+  VCPKG_INSTALLED_DIR='src/vcpkg_installed/arm64-osx'
+fi
 
 BUILD_DIR=cmake-build64
 BUILD_TYPE=Debug
 PROJECT_DIR=$(pwd)
-DRIVER_BIN_DIR="$PROJECT_DIR/build/odbc/bin"
-DRIVER_LOG_DIR="$PROJECT_DIR/build/odbc/logs"
 
-mkdir $DRIVER_LOG_DIR
+mkdir $PROJECT_DIR/build/odbc/logs
 
 mkdir $BUILD_DIR
 cd $BUILD_DIR
-cmake ../src -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DCODE_COVERAGE="ON" -DBUILD_SHARED_LIBS="OFF" -DWITH_TESTS="ON" -DWITH_ODBC="ON"
-make  -j 4
+cmake ../src -DCMAKE_INSTALL_PREFIX="${PROJECT_DIR}/${VCPKG_INSTALLED_DIR}" -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DCODE_COVERAGE="ON" -DBUILD_SHARED_LIBS="OFF" -DWITH_TESTS="ON" -DWITH_ODBC="ON"
+make -j 4
 
 RET_CODE=$?
 
 if [ $RET_CODE -ne 0 ]; then
-   echo "Error occurred while building macOS x64 project. Exiting."
-   exit $RET_CODE
+  echo "Error occurred while building macOS x64 project. Exiting."
+  exit $RET_CODE
 fi
 
 make package
@@ -48,8 +43,8 @@ make package
 RET_CODE=$?
 
 if [ $RET_CODE -ne 0 ]; then
-   echo "Error occurred while building macOS x64 installer. Exiting."
-   exit $RET_CODE
+  echo "Error occurred while building macOS x64 installer. Exiting."
+  exit $RET_CODE
 fi
 
 cd ..
